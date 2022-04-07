8fbdd7ef5060f0ba3fa63a6c4c1fcebf820375fb
Yizack Rangel <yizackr@gmail.com>
2022-04-07 07:24:28 -0500

    gists-readme

---
+++ README.md
@@ -0,0 +1,43 @@
+# GitHub Gists Readme (gists-readme)
+Display your Gists on your GitHub Readme.md
+
+## Live Demo
+Generate your link: https://gists-readme.yizack.com/
+
+## Usage/Examples 1
+User Yizack dislpay max, Dark Theme.
+```
+[![gists-readme](https://gists-readme.yizack.com/api?user=yizack&theme=dark)](https://gists-readme.yizack.com/api?user=yizack&theme=dark)
+```
+### Demo
+[![gists-readme](https://gists-readme.yizack.com/api?user=yizack&theme=dark)](https://gists-readme.yizack.com/api?user=yizack&theme=dark)
+
+## Usage/Examples 2
+User Yizack display 2 and no Dark Theme.
+```
+[![gists-readme](https://gists-readme.yizack.com/api?user=yizack&n=2)](https://gists-readme.yizack.com/api?user=yizack&n=2)
+```
+### Demo
+[![gists-readme](https://gists-readme.yizack.com/api?user=yizack&n=2)](https://gists-readme.yizack.com/api?user=yizack&n=2)
+
+## Usage/Examples 3
+User Cy4Bot, display 20 and no Dark Theme.
+```
+[![gists-readme](https://gists-readme.yizack.com/api?user=Cy4Bot&n=20)](https://gists-readme.yizack.com/api?user=Cy4Bot&n=20)
+```
+### Demo
+[![gists-readme](https://gists-readme.yizack.com/api?user=Cy4Bot&n=20)](https://gists-readme.yizack.com/api?user=Cy4Bot&n=20)
+
+
+
+## Parameters
+
+| Parameter | Description                           |
+| --------- | ------------------------------------- |
+| user      | Your GitHub profile username.         |
+| theme     | Select the theme `dark` (optional).   |
+| n         | Number of Gists to display (max 30).  |
+
+##
+Inspired by [anuraghazra/github-readme-stats](https://github.com/anuraghazra/github-readme-stats).\
+Made on [Vercel](https://vercel.com/) with Node.js.
---
+++ api/index.js
@@ -0,0 +1,110 @@
+const app = require('express')();
+const hbs = require('hbs');
+const axios = require('axios').default;
+const path = require("path");
+const fs = require('fs');
+const env = require('dotenv').config({ path: myPath(".env") });
+
+var languages = "";
+
+hbs.registerPartials(myPath("themes/partials"), function (err) {});
+app.set('view engine', 'hbs');
+app.set("views", myPath("themes"));
+
+app.get('/api/', async (req, res) => {
+  const { user } = req.query;
+  const { n } = req.query;
+  const { theme } = req.query;
+  const limit = isNaN(n) || typeof n === "undefined" || n === "";
+
+  console.log(n);
+
+  const token = process.env.token;
+  const options = {
+    headers: {
+      Authorization:  `token ${token}` 
+    }
+  };
+
+  languages = JSON.parse(fs.readFileSync(myPath('resources/language_colors.json'), 'utf8'));
+
+    var req = await axios.get(`https://api.github.com/users/${user}/gists`, options).then( async (response) => {
+    var gists = [];
+    var x = 0;
+    var y = 0;
+    var i = 0;
+    var nextLine = false;
+    
+    await response.data.forEach(function(d) {
+      if(limit || i < n){
+        var filename = Object.keys(d.files)[0];
+        var language = d.files[filename].language;
+        var color = getColor(language);
+        gists.push({
+          "file": filename,
+          "language": language,
+          "color": color,
+          "y" : y,
+          "x" : x
+        });
+
+        if(x == 200 && !nextLine){
+          x = 0;
+        }
+        else if (x ==0 && !nextLine) {
+          x = 200;
+          nextLine = true;
+        }
+
+        if(nextLine){
+          nextLine = false;
+          y = y - 25;
+        }
+        y = y + 25;
+        i++;
+      }
+    });
+
+    var height = y + 100;
+
+    var parameters = {
+      "gists": gists,
+      "height": height
+    }
+
+    res.setHeader("Content-Type", "image/svg+xml");
+
+    try {
+      if (fs.existsSync(myPath(`themes/${theme}.hbs`))) {
+        res.render(theme, parameters);
+      }
+      else {
+        throw error;
+      }
+    }
+    catch(e){
+      res.render('default', parameters);
+    }
+
+  }); 
+});
+
+
+function getColor(name){
+  try {
+    return languages[name].color;
+  }
+  catch(e){
+    return "#ededed";
+  }
+}
+
+function myPath(file){
+  return path.join(__dirname, "..", file);
+}
+
+module.exports = app;
+
+app.listen(1515, () => {
+  console.log(`Example app listening on port 1515`);
+})
