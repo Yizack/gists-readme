01156d0741bcf7fb7937a0b7217c7459321b0883
Yizack Rangel <yizackr@gmail.com>
2022-10-11 04:49:00 -0500

    Testing jest mock for axios and supertest for express

---
+++ tests/api.test.js
@@ -0,0 +1,8 @@
+import app from "../api/index.js";
+import request from "supertest";
+
+describe("renderCard", () => {
+  test("should render card", async () => {
+    await request(app).get("/api").expect("Content-Type", /svg/);
+  });
+});
--- tests/gists.test.js
+++ tests/gists.test.js
@@ -1,18 +1,67 @@
+import { jest } from "@jest/globals";
 import { getGists } from "../src/gists.js";
+import axios from "axios";
+
+jest.mock("axios");
+
+const fakeGist = {
+  data: [
+    {
+      files: {
+        "reduce_dataset.js": {
+          filename: "reduce_dataset.js",
+          language: "JavaScript",
+        },
+      },
+    },
+    {
+      files: {
+        "submissions.gs": {
+          filename: "submissions.gs",
+          language: "JavaScript",
+        },
+      },
+    },
+    {
+      files: {
+        "provincias.json": {
+          filename: "provincias.json",
+          language: "JSON",
+        },
+      },
+    }
+  ]
+};
+
+const fakeEmptyGist = {
+  data: []
+};
+
+axios.get = jest.fn();
 
 describe("getGists", () => {
-  
+
   test("yizack - should return gists", async () => {
+    axios.get.mockResolvedValue(fakeGist);
     const gists = await getGists("yizack");
-    expect(gists).toBeDefined();
+    expect(gists.data[0].files["reduce_dataset.js"].language).toEqual("JavaScript");
+    expect(gists.data[1].files["submissions.gs"].language).toEqual("JavaScript");
+    expect(gists.data[2].files["provincias.json"].language).toEqual("JSON");
   });
 
-  test("undefined - should return empty array", async () => {
+  test("undefined - should go on error and return empty data array", async () => {
+    axios.get.mockResolvedValue(Promise.reject(new Error()));
+    await getGists().catch((e) => {
+      expect(e).rejects.toThrow();
+    });
+
+    axios.get.mockResolvedValue(fakeEmptyGist);
     const gists = await getGists();
     expect(gists.data).toEqual([]);
   });
 
   test("user doesn't exist - should return empty array", async () => {
+    axios.get.mockResolvedValue(fakeEmptyGist);
     const gists = await getGists("_");
     expect(gists.data).toEqual([]);
   });
