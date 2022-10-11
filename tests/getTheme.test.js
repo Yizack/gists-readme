01857883fcba13866c32687801f893588372e471
Yizack Rangel <yizackr@gmail.com>
2022-10-11 14:51:23 -0500

    pin border color

--- tests/getTheme.test.js
+++ tests/getTheme.test.js
@@ -5,13 +5,15 @@ const themes = {
     "primary_color": "#0969da",
     "text_color": "#434d58",
     "bg_color": "#fffefe",
-    "icon_color": "#586069"
+    "icon_color": "#586069",
+    "border_color": "#e4e2e2"
   },
   dark: {
     "primary_color": "#58a6ff",
     "text_color": "#c9d1d9",
     "bg_color": "#0d1117",
-    "icon_color": "#58a6ff"
+    "icon_color": "#58a6ff",
+    "border_color": "#30363d"
   }
 };
 
