83b090984d6f9300b6e49bd1457b65dbc24742e5
Yizack Rangel <yizackr@gmail.com>
2022-10-11 20:08:36 -0500

    jsdom test environment

---
+++ tests/api.pin.test.js
@@ -0,0 +1,57 @@
+import { jest } from "@jest/globals";
+import app from "./../api/pin.js";
+import request from "supertest";
+import axios from "axios";
+jest.useFakeTimers();
+
+jest.mock("axios");
+
+const fakeResponse = {
+  user: "yizack",
+  id: "cbe7cef5572e6b832da0e9bd3454b312",
+  data: [
+    {
+      description: "description",
+      files: {
+        "reduce_dataset.js": {
+          filename: "reduce_dataset.js",
+          language: "JavaScript",
+        },
+      },
+    }
+  ],
+};
+
+const fakeHTML = {
+  data: `
+    <nav aria-label="Gist">
+      <div data-hotkey="g s">
+        <span class="Counter" title="1"></span>
+      </div>
+      <div data-hotkey="g f">
+        <span class="Counter" title="1">
+      </span></div>
+    </nav>`
+};
+
+const fakeHTML0 = {
+  data: `
+    <nav aria-label="Gist">
+    </nav>`
+};
+
+axios.get = jest.fn();
+
+describe("renderCard", () => {
+  test("should render card", async () => {
+    axios.get.mockResolvedValueOnce(fakeResponse);
+    axios.get.mockResolvedValueOnce(fakeHTML);
+    await request(app).get("/api/pin").expect("Content-Type", /svg/);
+  });
+
+  test("should render card with 0 stars and forks", async () => {
+    axios.get.mockResolvedValueOnce(fakeResponse);
+    axios.get.mockResolvedValueOnce(fakeHTML0);
+    await request(app).get("/api/pin").expect("Content-Type", /svg/);
+  });
+});
