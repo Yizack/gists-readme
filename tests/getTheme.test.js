99f83e2382801fe58be0030f7c7328844495fda5
Yizack Rangel <yizackr@gmail.com>
2022-10-17 05:11:25 -0500

    Remove hash symbol from the themes json and added them in the templates

--- tests/getTheme.test.js
+++ tests/getTheme.test.js
@@ -2,18 +2,18 @@ import { getTheme } from "./../src/functions.js";
 
 const themes = {
   default: {
-    "primary_color": "#0969da",
-    "text_color": "#434d58",
-    "bg_color": "#fffefe",
-    "icon_color": "#586069",
-    "border_color": "#e4e2e2"
+    "primary_color": "0969da",
+    "text_color": "434d58",
+    "bg_color": "fffefe",
+    "icon_color": "586069",
+    "border_color": "e4e2e2"
   },
   dark: {
-    "primary_color": "#58a6ff",
-    "text_color": "#c9d1d9",
-    "bg_color": "#0d1117",
-    "icon_color": "#58a6ff",
-    "border_color": "#30363d"
+    "primary_color": "58a6ff",
+    "text_color": "c9d1d9",
+    "bg_color": "0d1117",
+    "icon_color": "58a6ff",
+    "border_color": "30363d"
   }
 };
 
